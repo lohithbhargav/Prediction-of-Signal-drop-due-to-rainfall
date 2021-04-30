5d09e0a7e21bf7a2f7eea52b0eae5021df11e2a9
Lohith Bhargav <dlohithbhargav28@gmail.com>
2021-04-30 16:17:16 +0530

    static code analysis

--- Implementation/Data_Visualization2.py
+++ Implementation/Data_Visualization2.py
@@ -1,13 +1,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
 data=pd.read_csv("Implementation\Dataset_1.csv")
 X = np.array(data)[:,0].reshape(-1,1)
 x = np.array(data)[:,1].reshape(-1,1)
 y = np.array(data)[:,2].reshape(-1,1)
 z = np.array(data)[:,3].reshape(-1,1)
-
 plt.title('Data Visualization for Signal Loss at Light Rainfall Conditions')  
 plt.xlabel('Days')  
 plt.ylabel('Loss of Signal') 
